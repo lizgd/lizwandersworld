0c77e54105a4d1f29ea716281e778d6b190d5908
Elizabeth Dorman <elizabethdorman@MacBook-Air-5.local>
2025-08-05 23:07:33 -0700

    fix images

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true
   },
+  publicDir: 'assets',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
